93bd9c5cc96971e0b90c88c5b899692f8982d0c6
Gebryana <64340451+Gebryana@users.noreply.github.com>
2020-04-26 12:05:42 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utsnomor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Class1[] item = new Class1[3];
+
+
+
+
+
+            item[0] = new Class1("Jacket", 12, 59.95);
+
+
+            item[1] = new Class1("Jeans", 40, 34.95);
+
+
+            item[2] = new Class1("Shirt", 20, 24.95);
+
+
+
+
+
+            Console.WriteLine("=============================================");
+
+
+            Console.WriteLine("\tDescription\tUnits On Hand\tPrice");
+
+
+            Console.WriteLine("=============================================");
+
+
+            for (int i = 0; i < item.Length; i++)
+            {
+
+
+                Console.WriteLine("Item " + (i + 1) + "\t" + item[i].getDescription() + "\t\t" + item[i].getUnitsOnHand() + "\t\t" + item[i].getPrice());
+
+
+            }
+
+
+            Console.WriteLine("=============================================");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
